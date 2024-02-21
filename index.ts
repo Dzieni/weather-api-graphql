e14bd7c80f8eea828807fb1599a3459690b2a506
Michał Dzienisiewicz <michal.piotr.dz@gmail.com>
2024-02-21 03:11:12 +0100

    add auth and login/registration page

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { createYoga } from "graphql-yoga";
 import { schema } from "./graphql/schema.ts";
-import { WeatherApiClient, createClient } from "./weatherApi/client.ts";
-import { caching, Cache } from "cache-manager";
+import { createClient } from "./weatherApi/client.ts";
+import { caching } from "cache-manager";
+import oidc from "express-openid-connect";
+import { useGenericAuth } from "@envelop/generic-auth";
+
+const PORT = Number(process.env.PORT || 3000);
 
 const cache = await caching("memory", { ttl: 60 * 60 * 1000 });
 const weatherApi = createClient(
@@ -13,12 +17,32 @@ const weatherApi = createClient(
 
 const app = express();
 
-const yoga = createYoga({
+// adds /login, /logout, /callback
+app.use(
+	oidc.auth({
+		authRequired: false,
+		auth0Logout: true,
+		secret: process.env.OIDC_COOKIE_SECRET,
+		baseURL: process.env.BASE_URL || `http://localhost:${PORT}`,
+		clientID: process.env.OIDC_CLIENT_ID,
+		issuerBaseURL: process.env.OIDC_ISSUER_BASE_URL,
+	})
+);
+
+const yoga = createYoga<{ req: Express.Request }>({
 	schema,
-	context: { weatherApi, cache },
+	context: ({ req }) => ({ weatherApi, cache, user: req.oidc.user }),
+	plugins: [
+		useGenericAuth({
+			resolveUserFn: ({ user }) => user,
+			mode: "protect-all",
+		}),
+	],
 });
 app.use(yoga.graphqlEndpoint, yoga);
 
-app.listen(3000, () => {
-	console.log("Running a GraphQL API server at http://localhost:3000/graphql");
+app.listen(PORT, () => {
+	console.log(
+		`Running a GraphQL API server at http://localhost:${PORT}/graphql`
+	);
 });
