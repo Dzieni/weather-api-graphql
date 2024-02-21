18418ff10d1a9ca83e3e73b81e6c91a265ade918
Michał Dzienisiewicz <michal.piotr.dz@gmail.com>
2024-02-21 02:15:13 +0100

    weatherApi: add caching

--- weatherApi/client.ts
+++ weatherApi/client.ts
@@ -1,6 +1,11 @@
-import createOpenApiClient, { ClientOptions, Middleware } from "openapi-fetch";
+import createOpenApiClient, {
+	ClientMethod,
+	ClientOptions,
+	Middleware,
+} from "openapi-fetch";
 import type { paths } from "./__types__.ts";
-import { caching } from "cache-manager";
+import { Cache, WrapTTL } from "cache-manager";
+import stableStringify from "json-stable-stringify";
 
 export const DEFAULT_BASE_URL = "https://api.weatherapi.com/v1";
 export type WeatherApiClient = ReturnType<typeof createClient>;
@@ -16,22 +21,51 @@ const authMiddleware = (apiKey: string): Middleware => {
 	};
 };
 
+const defaultTtl: WrapTTL<Awaited<ReturnType<ClientMethod<paths, "get">>>> = ({
+	response,
+	error,
+}) => {
+	if (error) return 0;
+
+	if (response.url.includes("/current.json")) {
+		return 1 * 60 * 1000; // 1 minute;
+	}
+
+	if (response.url.includes("/history.json")) {
+		return 60 * 60 * 1000; // 1 hour;
+	}
+
+	return 15 * 60 * 1000; // 15 minutes;
+};
+
 export const createClient = (
 	apiKey: string,
 	baseUrl: string = DEFAULT_BASE_URL,
-	options?: Omit<ClientOptions, "baseUrl">
+	options?: Omit<ClientOptions, "baseUrl"> & {
+		cacheManager?: Cache;
+		ttl?: typeof defaultTtl;
+	}
 ) => {
+	const { cacheManager, ttl = defaultTtl, ...restOptions } = options ?? {};
 	const client = createOpenApiClient<paths>({
-		...options,
+		...restOptions,
 		baseUrl,
 	});
 
 	client.use(authMiddleware(apiKey));
 
+	if (cacheManager) {
+		const origGet = client.GET;
+		client.GET = ((...args: Parameters<typeof origGet>) => {
+			const [endpoint, options] = args;
+			const cacheKey = stableStringify([endpoint, options.params]);
+			return cacheManager.wrap(
+				cacheKey,
+				() => origGet.apply(client, args),
+				ttl
+			);
+		}) as typeof origGet;
+	}
+
 	return client;
 };
-
-const memoryCache = await caching("memory", {
-	max: 100,
-	ttl: 10 * 1000 /*milliseconds*/,
-});
